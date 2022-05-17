fd3932fb2f9c400f9cf50ffd1d181b62f9f914fc
kessisdiones <73185354+kessisdiones@users.noreply.github.com>
2022-05-16 21:56:10 -0300

    a

--- New Unity Project/Assets/Scripts/AtaquePlayer.cs
+++ New Unity Project/Assets/Scripts/AtaquePlayer.cs
@@ -16,15 +16,23 @@ public class AtaquePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Atacar();
         if(Time.time - lastClickedTime > MaxComboDelay){
             noOfClicks = 0;
         
         }
-        if(Input.GetMouseButtonDown(0)){
+        
+    }
+    public void Atacar(){
+        if(Input.GetMouseButtonUp(0)){
             lastClickedTime = Time.time;
             noOfClicks++;
             if(noOfClicks == 1){
                 anim.SetBool("Ataque1", true);
+            }
+            else{
+                anim.SetBool("Ataque1", false);
+                
             }
             noOfClicks = Mathf.Clamp(noOfClicks,0,1);
         }
